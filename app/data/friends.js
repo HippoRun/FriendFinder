d7f99cc54b9d7aa1f7d383560d8a582ce4fec590
HippoRun <mayradlopezh@gmail.com>
2018-08-19 22:46:39 -0600

    html and friends now have syntax

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/4shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+           5 ,
+           1,
+           4,
+           4,
+           5,
+           1,
+           2,
+           5,
+           4,
+           1
+        ]
+    }
+];
+
+module.exports = friendsArray;
