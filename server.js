c6d21660ebcbe751bbfe28ee46d37c209fb6e5c0
HippoRun <mayradlopezh@gmail.com>
2018-08-20 21:48:55 -0600

    test4

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urledcoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
